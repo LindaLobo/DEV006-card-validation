6b9b9ab307f37ec66af6f3d32482154cdd33a587
Linda <lindalobo.aguilar@gmail.com>
2023-03-23 14:16:36 -0300

    validacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+document.querySelector(".card-number-input").oninput = () => {
+  const datos = document.querySelector(".card-number-input").value;
+  const resultado = validator.maskify(datos);
+  document.querySelector(".card-number-box").innerText = resultado;
+};
+
+document.querySelector(".card-holder-input").oninput = () => {
+  document.querySelector(".card-holder-name").innerText = document
+    .querySelector(".card-holder-input")
+    .value.toUpperCase();
+};
+
+document.querySelector(".input-mes").oninput = () => {
+  document.querySelector(".expiration-mm").innerText =
+    document.querySelector(".input-mes").value;
+};
+
+document.querySelector(".input-year").oninput = () => {
+  document.querySelector(".expiration-yy").innerText =
+    document.querySelector(".input-year").value;
+};
+
+document.querySelector(".cvv-input").oninput = () => {
+  document.querySelector(".cvv-box").innerText =
+    document.querySelector(".cvv-input").value;
+};
+
+const boton = document.getElementById("nuevo_boton");
+
+const contenedor = document.getElementById("numberTarjeta");
+
+boton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const container = contenedor.value;
+  console.log(container);
+  validator.maskify(container);
+  validator.isValid(container);
+  const response = validator.isValid(container);
+  if (response === true) {
+    document.getElementById("validador").innerText = "Su tarjeta paso, GRACIAS por la Donación";
+  } else {
+    document.getElementById("validador").innerText =
+    "Verificar los datos de su Tarjeta";
+  }
+});
+
+/* console.log(validator); */
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
+  isValid: (container) => {
+    const convertAndReverse = container.toString().split("").reverse().join("");
+    console.log(convertAndReverse);
+
+    // multiplicacion
+    const numero = convertAndReverse.split("");
+    const multip = 2;
+
+    const multiplicar = numero.map(function (n, index) {
+      if (index % 2 !== 0) {
+        const par = n * multip;
+        console.log(par);
+        console.log(n, index);
+        if (par > 9) {
+          const suma = par
+            .toString()
+            .split("")
+            .reduce((acumulador, item) => {
+              return acumulador + parseInt(item);
+            }, 0);
+          console.log("si hay", par);
+          return suma;
+        } else {
+          console.log("no", par);
+          return par;
+        }
+      } else {
+        return n;
+      }
+    });
+    const total = multiplicar.reduce((acumula, item) => {
+      return acumula + parseInt(item);
+    }, 0);
+    console.log(total);
+    if (total % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify: (container) => {
+    const clave =
+      container.slice(0, -4).replace(/./g, "#") + container.slice(-4);
+    console.log(clave);
+    return clave;
+  },
 };
 
 export default validator;
